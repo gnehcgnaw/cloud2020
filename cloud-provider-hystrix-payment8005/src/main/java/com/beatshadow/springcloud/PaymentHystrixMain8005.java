2024b4cd83c626860a786edde4f73a3a805f64c0
gnehcgnaw <gnehcgnaw@gamil.com>
2020-04-25 01:53:57 +0800

    使用@HystrixCommand完成服务降级，后续会优化。

--- cloud-provider-hystrix-payment8005/src/main/java/com/beatshadow/springcloud/PaymentHystrixMain8005.java
+++ cloud-provider-hystrix-payment8005/src/main/java/com/beatshadow/springcloud/PaymentHystrixMain8005.java
@@ -2,12 +2,15 @@ package com.beatshadow.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author : <a href="mailto:gnehcgnaw@gmail.com">gnehcgnaw</a>
  * @since : 2020/4/20 23:54
  */
+
+@EnableCircuitBreaker   //开启熔断
 @EnableEurekaClient
 @SpringBootApplication
 public class PaymentHystrixMain8005 {
--- cloud-provider-hystrix-payment8005/src/main/java/com/beatshadow/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8005/src/main/java/com/beatshadow/springcloud/service/PaymentService.java
@@ -1,5 +1,8 @@
 package com.beatshadow.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.contrib.javanica.conf.HystrixPropertiesManager;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -25,13 +28,31 @@ public class PaymentService{
      * 模拟超时
      * @return
      */
+    @HystrixCommand(
+            //服务降级方法
+            fallbackMethod ="paymentInfo_Timeout_fallbackHandler" ,commandProperties = {
+            //什么时候进行超时处理
+            @HystrixProperty(name = HystrixPropertiesManager.EXECUTION_ISOLATION_THREAD_TIMEOUT_IN_MILLISECONDS,value = "2000")
+    })
     public String paymentInfo_Timeout(Integer id){
+        //有异常，或者超时都会进行服务降级
+        //int count = 10/0 ;
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return Thread.currentThread().getName()+"...paymentInfo_Timeout"+"...OK .";
+        return Thread.currentThread().getName()+"...paymentInfo_Timeout"+id+"...OK .";
     }
+
+    /**
+     * 服务降级方案
+     * @param id
+     * @return
+     */
+    public String paymentInfo_Timeout_fallbackHandler(Integer id){
+        return Thread.currentThread().getName()+"...paymentInfo_Timeout_fallbackHandler"+id+"...OK .";
+    }
+
 
 }
